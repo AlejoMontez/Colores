974f2f7ec5b245fa8d13d594bcb06c569131dd30
pc709 <pc709@iucesmag.edu.co>
2018-04-05 13:08:03 -0500

    Cambio 2

--- app/src/main/java/com/example/aula7/colores/MainActivity.java
+++ app/src/main/java/com/example/aula7/colores/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.aula7.colores;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //declarando el atributo
+ TextView textViewName;
+ Button buttonOpen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewName = (TextView)findViewById(R.id.id_tv);
+        buttonOpen = (Button) findViewById(R.id.id_tbn_open);
+
+
+
+
+
+
+        buttonOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewName.setText("Gabriel Montezuma");
+                Toast.makeText(MainActivity.this, "Hola mundo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_index, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.id_item_menu_1:
+                Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.id_item_menu_2:
+                Toast.makeText(this, "Item 3", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
